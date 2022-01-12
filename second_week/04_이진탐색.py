9284b4027ebd3437eac67c88a31213b2b56dc403
pkyh2 <pkyh1@naver.com>
2022-01-13 00:27:43 +0900

    2week

--- second_week/04_이진탐색.py
+++ second_week/04_이진탐색.py
@@ -50,38 +50,3 @@ for t in range(int(input())):
     count=1
     makeTree(1)
     print('#{} {} {}'.format(t+1, tree[1], tree[N//2]))
-
-
-
-  # 재귀로 표현
-def checkchild(start) :
-    global N
-    if start*2 <= N :
-        array[start][1] = start*2
-        checkchild(start*2)
-    if start*2+1 <= N :
-        array[start][2]=start*2+1
-        checkchild(start*2+1)
-
-# 시작점은 1번 노드
-def inorder(start):
-    global n
-    if start :  
-        inorder(array[start][1])
-        array[start][0]=n
-        n+=1
-        inorder(array[start][2])
-
-# 코드 구현
-T=int(input())
-for tc in range(T):
-    N=int(input())
-
-    array=[[0 for _ in range(3)] for __ in range(N+1) ] # 내 값, 왼쪽 자식, 오른쪽 자식
-    checkchild(1)   # 1번부터 자식 있는 것까지 다 표시
-
-    n=1
-    inorder(1)
-
-    # 결과 출력
-    print("#%d" %(tc+1), array[1][0], array[N//2][0])
