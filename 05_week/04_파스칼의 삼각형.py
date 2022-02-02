ecc39f0c5968ee23073b6221e8c46a9f8ba1c2a8
pkyh2 <pkyh1@naver.com>
2022-02-02 16:39:18 +0900

    5week

---
+++ 05_week/04_파스칼의 삼각형.py
@@ -0,0 +1,21 @@
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    pascal = [[0] * N for _ in range(N)]    # N * N 리스트 0으로 초기화
+
+    for y in range(N):
+        for x in range(N):
+            if x == 0:
+                pascal[y][x] = 1
+            elif x == y:
+                pascal[y][x] = 1
+            elif y > x:
+                pascal[y][x] = pascal[y-1][x-1] + pascal[y-1][x]
+
+    print('#{}'.format(t+1))
+    for i in range(N):
+        for j in range(N):
+            if pascal[i][j] != 0:
+                print(pascal[i][j], end=' ')
+        print()
