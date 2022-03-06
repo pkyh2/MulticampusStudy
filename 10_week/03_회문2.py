1fd729e84070e08c132e52a5b036411961fb5410
pkyh2 <pkyh1@naver.com>
2022-03-06 15:09:47 +0900

    10week

--- 10_week/03_회문2.py
+++ 10_week/03_회문2.py
@@ -1,20 +1,20 @@
-for t in range(1):
+def palindrome(board):
+    global maxLen
+    for y in range(100):
+        for x in range(100):
+            for i in list(range(100, 1, -1)):   # 가장 긴 회문이니까 큰 수부터
+                if board[y][x:i] == board[y][i-1:x-1:-1]:
+                    if maxLen < i-x:
+                        maxLen = i-x
+
+for t in range(10):
     T = int(input())
     board = [list(input()) for _ in range(100)]
-    len = list(range(1, 101))
     maxLen = 0
-    # 회문을 찾으면 다음 길이로 넘어감
-    for y in range(100):
-        for x in range(100):
-            for i in len:    # 회문의 길이
-                if board[y][x:i] == board[y][i-1::-1]:
-                    print(i)
-                    # maxLen = len(board[y][x:i])
-
-    print()
+    # 가로줄
+    palindrome(board)
+    # 세로줄
     zipboard = list(map(list, zip(*board)))
-    for y in range(100):
-        for x in range(100):
-            for i in len:    # 회문의 길이
-                if zipboard[y][x:i] == zipboard[y][i-1::-1]:
-                    print(i)
+    palindrome(zipboard)
+
+    print("#{} {}".format(T, maxLen))
