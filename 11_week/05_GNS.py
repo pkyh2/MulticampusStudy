617cc3cd108cf7d6da5e6cf95856030893205023
pkyh2 <pkyh1@naver.com>
2022-03-15 22:02:00 +0900

    11week

---
+++ 11_week/05_GNS.py
@@ -0,0 +1,15 @@
+numbers = {"ZRO":0, "ONE":1, "TWO":2, "THR":3, "FOR":4, "FIV":5, "SIX":6, "SVN":7, "EGT":8, "NIN":9}
+T = int(input())
+for t in range(T):
+    n, num = map(str, input().split())
+    numStr = input().split()
+    strToNum = []
+    for i in numStr:
+        for key, value in numbers.items():
+            if i == key:
+                strToNum.append((value, key))
+    strToNum.sort()
+    
+    print(n)
+    for i in strToNum:
+        print(i[1], end=' ')
