98ec79716ff926fdc30722912026f09933d5d191
pkyh2 <pkyh1@naver.com>
2022-03-27 15:35:30 +0900

    13week

--- 13_week/02_수의 새로운 연산.py
+++ 13_week/02_수의 새로운 연산.py
@@ -1,27 +1,31 @@
-def coordinate (p, q):
-    # 좌표 평면 생성
-    plane = [[] for _ in range(142)]
-    cnt = 1
-    for i in range(142):
-        for j in range(i+1):
-            plane[i].insert(0, cnt)
-            cnt += 1
-    
-    # 문제 풀이
-    comma = []
-    for i in range(142):
-        for j in range(i+1):
-            if plane[i][j] == p:
-                comma.append([i, j])
-            if plane[i][j] == q:
-                comma.append([i, j])
-                break
-    
-    sumComma = (comma[0][0]+comma[0][1], comma[1][0]+comma[1][1])
-    print(sumComma)
-    return plane[sumComma[0]][sumComma[1]]
+'''
+수열의 합으로 해당 라인의 처음 수와 끝 수를 구한다.
+처음 수의 좌표는 (1, 해당라인의 개수)
+'''
+def comma(num):
+    sum, i = 0, 1
+    while sum < num:
+        sum += i
+        i += 1
+
+    n = i - 1
+    start = 1 + ((n-1)*n)//2
+    end = (n*(n+1))//2
+    x = num - start + 1
+    y = end - num + 1
+    return [x, y]
 
 T = int(input())
 for t in range(T):
     p, q = map(int, input().split())
-    print('#{} {}'.format(t+1, coordinate(p, q)))
+    commaSum = []
+    commaSum.append(comma(p))
+    commaSum.append(comma(q))
+    commaSum.append(commaSum[0][0]+commaSum[1][0])
+    commaSum.append(commaSum[0][1]+commaSum[1][1])
+    # print(commaSum)
+
+    n = commaSum[2] + commaSum[3]
+    end = 1+ (n*(n-1))//2
+    answer = end - commaSum[3]
+    print('#{} {}'.format(t+1, answer))
