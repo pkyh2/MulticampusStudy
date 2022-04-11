21f0c4d703ff3a2893a54c73d9f28a1fc08ab647
pkyh2 <pkyh1@naver.com>
2022-04-11 20:06:41 +0900

    15week

---
+++ 15_week/01_구구단 1.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for t in range(T):
+    num = int(input())
+    flag = 0
+    for i in range(1, 10):
+        if num / i < 10 and (num // i) * i == num:
+            print('#{} {}'.format(t+1, 'Yes'))
+            flag = 1
+            break
+    if flag == 0:
+        print('#{} {}'.format(t+1, 'No'))
---
+++ 15_week/02_이진수 표현.py
@@ -0,0 +1,26 @@
+'''
+정수 N, M이 주어질 때, M의 이진수 표현의 마지막 N비트가 모두 1로 켜져 있는지 판별
+'''
+# T = int(input())
+
+# for t in range(T):
+#     N, M = map(int, input().split())
+#     bin = list(format(M, 'b'))[-N:]
+#     if '0' in bin:
+#         print('#{} {}'.format(t+1, 'OFF'))
+#     else:
+#         print('#{} {}'.format(t+1, 'ON'))
+
+
+T = int(input())
+
+for t in range(T):
+    N, M = map(int, input().split())
+    check = ''
+    for _ in range(N):
+        if M % 2 == 1:
+            M //= 2
+            check = "ON"
+        else:
+            check = "OFF"
+    print('#{} {}'.format(t+1, check))
