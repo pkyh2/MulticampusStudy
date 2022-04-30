b5f92ec5aaef964b7e7d95b993dae90d6c8441fa
pkyh2 <pkyh1@naver.com>
2022-04-30 17:08:19 +0900

    16week

--- 16_week/숫자 조작.py
+++ 16_week/숫자 조작.py
@@ -1,5 +1,45 @@
 T = int(input())
 
 for t in range(T):
-    N = list(int(input()))
-    if 
+    num = int(input())
+    N = list(map(int, str(num)))
+    flag = 1
+    # 최소값
+    if N[0] != max(N):
+        minNum = num
+        temp = N[0]
+        maxIdx = N.index(max(N))
+        N[0] = max(N)
+        N[maxIdx] = temp
+        maxNum = int(''.join(list(map(lambda x:str(x), N))))
+    # 최댓값이면
+    else:
+        maxNum = num
+        if min(N) == 0:
+            tp = N
+            while min(tp) == 0:
+                del tp[tp.index(min(N))]
+                if len(tp) <= 1:
+                    minNum = num
+                    print('# {} {} {}'.format(t+1, minNum, maxNum))
+                    flag = 0
+                    break
+            tpMin = min(tp)
+        
+            temp = N[0]
+            minIdx = N.index(tpMin)
+            N[0] = tpMin
+            N[minIdx] = temp
+            minNum = int(''.join(list(map(lambda x:str(x), N))))
+        else:
+            maxNum = num
+            temp = N[0]
+            minIdx = N.index(min(N))
+            N[0] = min(N)
+            N[minIdx] = temp
+            minNum = int(''.join(list(map(lambda x:str(x), N))))
+
+    if flag == 1:
+        print('# {} {} {}'.format(t+1, minNum, maxNum))
+    
+# 모르겠다 ㅜㅜ
--- 16_week/정사각형 판정.py
+++ 16_week/정사각형 판정.py
@@ -1,10 +1,27 @@
 T = int(input())
 
 for t in range(T):
-    boardLen = int(input())
-    board = [[list(map(str, input().split()))] for _ in range(boardLen)]
-    p1, p2, p3, p4 = [0, 0], [0, 0], [0, 0], [0, 0]
+    boardLine = int(input())
+    board = [[list(map(str, input().split()))] for _ in range(boardLine)]
+    # p1, p2, p3, p4 = [0, 0], [0, 0], [0, 0], [0, 0]
+    start = []
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == '#':
-                if 
+                start.append(i)
+                start.append(j)
+                break
+        break
+
+    row, col = 1, 1
+    for i in range(len(board) - start[i]):
+        if board[start[0]][start[1]+row] == '#' and board[start[0]+col][start[1]] == '#':
+            row += 1
+            col += 1
+            if row != col:
+                print('#{} {}'.format(t+1, 'NO'))
+                break
+        else:
+            print('#{} {}'.format(t+1, 'YES'))
+            break
+# 모르겠다 ㅜㅜ
